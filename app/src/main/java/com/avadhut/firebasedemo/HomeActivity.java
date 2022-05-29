96165d3f27e5700145eb58f67f4c253d099bbed2
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2022-05-29 19:21:49 +0530

    add: google sign in

--- app/src/main/java/com/avadhut/firebasedemo/HomeActivity.java
+++ app/src/main/java/com/avadhut/firebasedemo/HomeActivity.java
@@ -2,13 +2,55 @@ package com.avadhut.firebasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // view initialization
+        ImageView userImageView = findViewById(R.id.userIconImgView);
+        TextView userNameTextView = findViewById(R.id.userNameTextView);
+
+        // firebase initialization
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if (currentUser != null) {
+            // get photo image url, name, email from currentUser object
+            Uri photoUri = currentUser.getPhotoUrl();
+            String imageURL = photoUri != null ? photoUri.toString() : "";
+            String userName = "Name: " + currentUser.getDisplayName();
+            String userEmail = "Email: " + currentUser.getEmail();
+
+            // set user name & email
+            userNameTextView.setText(userName + "\n" + userEmail);
+
+            // set profile image
+            Glide.with(this)
+                    .load(imageURL)
+                    .circleCrop()
+                    .into(userImageView);
+        } else {
+            Toast.makeText(this, "User not logged in, please login", Toast.LENGTH_SHORT).show();
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+            finishAfterTransition();
+        }
+
     }
 }
