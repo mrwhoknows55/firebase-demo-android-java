0404eb6d716ce0e69418aaa2dfe970c331ed5186
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2022-05-29 18:17:33 +0530

    add: check for existing login

--- app/src/main/java/com/avadhut/firebasedemo/MainActivity.java
+++ app/src/main/java/com/avadhut/firebasedemo/MainActivity.java
@@ -4,25 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean isLoggedIn = false;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // check if user is logged in or not and change value of isLoggedIn accordingly
-        if (isLoggedIn) {
-            Intent homeIntent = new Intent(this, HomeActivity.class);
-            startActivity(homeIntent);
-            finishAfterTransition();
-        } else {
-            Intent loginIntent = new Intent(this, LoginActivity.class);
-            startActivity(loginIntent);
-            finishAfterTransition();
-        }
+        firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        isLoggedIn = user != null;
+
+        new Handler().postDelayed(() -> {
+            if (isLoggedIn) {
+                Intent homeIntent = new Intent(this, HomeActivity.class);
+                startActivity(homeIntent);
+                finishAfterTransition();
+            } else {
+                Intent loginIntent = new Intent(this, LoginActivity.class);
+                startActivity(loginIntent);
+                finishAfterTransition();
+            }
+        }, 2_000L);
+
     }
 }
