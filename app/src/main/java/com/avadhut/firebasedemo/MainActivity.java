c98bd7213bfa346b67d63307a47e2b0866a9537a
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2022-05-29 17:55:41 +0530

    add: basic user interface elements 🎨

--- app/src/main/java/com/avadhut/firebasedemo/MainActivity.java
+++ app/src/main/java/com/avadhut/firebasedemo/MainActivity.java
@@ -2,13 +2,27 @@ package com.avadhut.firebasedemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isLoggedIn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // check if user is logged in or not and change value of isLoggedIn accordingly
+        if (isLoggedIn) {
+            Intent homeIntent = new Intent(this, HomeActivity.class);
+            startActivity(homeIntent);
+            finishAfterTransition();
+        } else {
+            Intent loginIntent = new Intent(this, LoginActivity.class);
+            startActivity(loginIntent);
+            finishAfterTransition();
+        }
     }
 }
