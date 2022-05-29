8648f7eb5b9c97a57a0dd9e3b440af926fadc24a
Avadhut Tanugade <avdhuttanugade55@gmail.com>
2022-05-29 18:55:58 +0530

    add: check for existing login

--- app/src/main/java/com/avadhut/firebasedemo/LoginActivity.java
+++ app/src/main/java/com/avadhut/firebasedemo/LoginActivity.java
@@ -1,14 +1,78 @@
 package com.avadhut.firebasedemo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+    private GoogleSignInClient googleSignInClient;
+    public static final int SIGN_IN_RESULT_CODE = 420;
+    private static final String OAUTH_ID = "YOUR_TOKEN";
+
     @Override
+    @SuppressWarnings({"deprecation"})
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(OAUTH_ID).requestEmail().build();
+        googleSignInClient = GoogleSignIn.getClient(this, gso);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        Button signInButton = findViewById(R.id.btnSignIn);
+        signInButton.setOnClickListener(v -> {
+            Intent signInIntent = googleSignInClient.getSignInIntent();
+            startActivityForResult(signInIntent, SIGN_IN_RESULT_CODE);
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            Task<GoogleSignInAccount> signInTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+            if (signInTask.isSuccessful()) {
+                try {
+                    GoogleSignInAccount account = signInTask.getResult(ApiException.class);
+                    firebaseAuthWithGoogle(account.getIdToken());
+                } catch (Throwable e) {
+                    Toast.makeText(this, "Error: " + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener(this, task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(this, "Login successful!", Toast.LENGTH_SHORT).show();
+                Intent homeIntent = new Intent(this, HomeActivity.class);
+                startActivity(homeIntent);
+                finishAfterTransition();
+            } else {
+                Toast.makeText(this, "Login failed: " + task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
